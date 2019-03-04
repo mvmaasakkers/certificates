d04dfd832ed684dfbe1693b4f90df34d934a43f6
Martijn van Maasakkers <martijn@newestindustry.nl>
2019-03-04 08:29:25 +0100

    Updated usage name and description of cli

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "Certificates"
+	app.Usage = ""
 	app.Version = "v0.0.1-alpha1"
+	app.Description = "An opinionated TLS certificate generator."
 	app.Commands = []cli.Command{
 		commands.CertificateCommand,
 	}
